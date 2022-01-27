a187f3a41f66f6432c95d3d87cfcc14037a3a92c
Raul Perez <raulytstation@outlook.com>
2022-01-27 11:52:42 -0400

    updated bspwm

--- dotfiles/config/lvim/config.lua
+++ dotfiles/config/lvim/config.lua
@@ -7,7 +7,7 @@ a global executable or a path to
 an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
+vim.opt.shell = "/bin/sh"
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
@@ -135,22 +135,26 @@ lvim.builtin.treesitter.highlight.enabled = true
  }
 
 -- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
+ local linters = require "lvim.lsp.null-ls.linters"
+ linters.setup {
 --   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
+   {
+     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+     command = "shellcheck",
 --     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+     extra_args = { "--severity", "warning" },
+   },
+   {
+     command = "codespell",
+     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+     filetypes = { "javascript", "python" },
+   },
+  {
+    command = "eslint",
+    filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+  }
+ }
 
 -- Additional Plugins
 lvim.plugins = {
@@ -235,3 +239,4 @@ lvim.plugins = {
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+require("luasnip.loaders.from_vscode").load()
