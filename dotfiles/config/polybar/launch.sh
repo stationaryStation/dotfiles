48c0c69f2085b4836a4865587d458a2ab7d0fad2
StationaryStation <foxxo@pop-os.localdomain>
2022-02-19 21:37:50 -0400

    Imported new I3 Config

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-polybar top -c ~/.config/polybar/config.ini &
+# Launch
+polybar main &
+polybar top &
+
+echo "Bar launched..."
